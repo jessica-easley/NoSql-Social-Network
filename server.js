924326133cc5da46a62311c3f371ded6c9a7503b
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-04-02 21:47:22 -0500

    updated routes and updated ser.js files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require(',/routes')
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(routes);
+
+mongoose.connect('mongodb://127.0.0.1/social-network-api', {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+.then(() => console.log('MongoDB is connected!'))
+.catch((err) => console.error(err));
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log('Now Listening on Port ${PORT}'));
